7a3679ecef4f2b88a6432121b0bba473b5c041c9
Andrey-Nekrasov-coder <asklshadow8088@gmail.com>
2022-09-03 14:16:35 +0700

    HomeWork Is Ready

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= n; i+=2)
+{
+if (i == n || i == n - 1)
+Console.Write(i);
+else
+Console.Write(i + ", ");
+}
+
